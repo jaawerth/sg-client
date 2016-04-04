65e16f89ea40b11be2b8816a80cc3a6ac50ff8c8
Jesse Wertheim <jaawerth@users.noreply.github.com>
2016-04-04 16:16:06 -0400

    changed eslintrc format

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "parser": "babel-eslint",
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "es6": true,
+    "node": true
+  },
+  "extends": "jw",
+  "rules": {
+    "indent": [
+        2,
+        2
+    ],
+    "linebreak-style": [
+        2,
+        "unix"
+    ]
+  },
+  ecmaFeatures: {
+    experimantalObjectRestSpread: true
+  }
+};
